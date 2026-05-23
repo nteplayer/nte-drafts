2cba9ec391c3c518245c2b8a8dbd9b2a723f4e58
nteplayer <rusin.dima1103@gmail.com>
2026-05-23 22:49:33 +0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className='flex h-full items-center justify-center'>
+      <h1 className='text-5xl font-black'>
+        Draft Platform
+      </h1>
+    </div>
+  )
+}
