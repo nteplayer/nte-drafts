355c1aece8ae1252f9b1ecf19b4096f0a13a24bf
nteplayer <rusin.dima1103@gmail.com>
2026-05-23 22:48:39 +0500

    Create authStore.ts

---
+++ src/store/authStore.ts
@@ -0,0 +1,12 @@
+import { create } from 'zustand'
+
+interface AuthState {
+  user: any
+  setUser: (user: any) => void
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+
+  setUser: (user) => set({ user })
+}))
