2d140ed4a058ec44c32533c1c84913a32667919c
nteplayer <rusin.dima1103@gmail.com>
2026-05-24 10:28:09 +0500

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,12 +9,19 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+
+  apiKey: "AIzaSyAa_zvBCLyFZKcclMtUfRs_2Tx__JXcTP4",
+
+  authDomain: "nte-tournaments-7b0e3.firebaseapp.com",
+
+  projectId: "nte-tournaments-7b0e3",
+
+  storageBucket: "nte-tournaments-7b0e3.appspot.com",
+
+  messagingSenderId: "1022034023092",
+
+  appId: "1:1022034023092:web:d269068348b24abed93de1"
+
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,8 +29,13 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export {
+
   createUserWithEmailAndPassword,
+
   signInWithEmailAndPassword,
+
   signOut,
+
   onAuthStateChanged
+
 };
