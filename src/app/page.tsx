2252c4f99260d9a282c8d7e271a9d0c2fc4e3874
nteplayer <rusin.dima1103@gmail.com>
2026-05-26 18:58:14 +0500

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className='flex h-full items-center justify-center'>
-      <h1 className='text-5xl font-black'>
-        Draft Platform
-      </h1>
-    </div>
-  )
-}
